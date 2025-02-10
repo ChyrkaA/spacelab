00f047be1f1e71bb00b193f961314285fd192a49
Andriy Chirka <andrey.chirka@gmail.com>
2025-02-10 21:42:30 +0200

    update

--- js/main.js
+++ js/main.js
@@ -25,16 +25,48 @@ document.addEventListener("DOMContentLoaded", () => {
 				loop: false,
 				centeredSlides: false,
 				navigation: {
-					nextEl: ".custom-button-next", // Кнопка "вперед"
-					prevEl: ".custom-button-prev", // Кнопка "назад"
+					nextEl: ".custom-button-next",
+					prevEl: ".custom-button-prev",
 				},
 				breakpoints: {
 					768: {
-						spaceBetween: 20, // На планшетах чуть шире
+						spaceBetween: 20,
 					},
 				},
 			});
 		}
 	}
 	window.addEventListener("resize", initSwiper);
+
+	if (window.innerWidth < 992) {
+		const bigMenu = document.querySelector(".menu__bigMenu");
+		const hambudger = document.querySelector(".hamburger");
+		function action(action) {
+			hambudger.addEventListener(action, () => {
+				bigMenu.classList.toggle("showMenu");
+				hambudger.classList.toggle("cross");
+			});
+		}
+		action("click");
+	}
+
+	window.addEventListener("load", function () {
+		console.log(navigator.userAgent);
+		if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+			const videos = document.querySelectorAll("video");
+
+			videos.forEach((video) => {
+				const videoClass = video.className;
+				const webmSource = video.querySelector('source[type="video/webm"]');
+				if (webmSource) {
+					const gifSrc = webmSource.src.replace(".webm", ".gif");
+					const img = document.createElement("img");
+					img.src = gifSrc;
+					img.alt = "coin";
+					img.className = videoClass;
+					video.parentNode.replaceChild(img, video);
+				}
+			});
+		}
+	});
 });
--- js/token.js
+++ js/token.js
@@ -79,4 +79,34 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 	window.addEventListener("resize", initSwiper);
+	if (window.innerWidth < 992) {
+		const bigMenu = document.querySelector(".menu__bigMenu");
+		const hambudger = document.querySelector(".hamburger");
+		function action(action) {
+			hambudger.addEventListener(action, () => {
+				bigMenu.classList.toggle("showMenu");
+				hambudger.classList.toggle("cross");
+			});
+		}
+		action("click");
+	}
+	window.addEventListener("load", function () {
+		console.log(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+		if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+			const videos = document.querySelectorAll("video");
+
+			videos.forEach((video) => {
+				const videoClass = video.className;
+				const webmSource = video.querySelector('source[type="video/webm"]');
+				if (webmSource) {
+					const gifSrc = webmSource.src.replace(".webm", ".gif");
+					const img = document.createElement("img");
+					img.src = gifSrc;
+					img.alt = "coin";
+					img.className = videoClass;
+					video.parentNode.replaceChild(img, video);
+				}
+			});
+		}
+	});
 });
